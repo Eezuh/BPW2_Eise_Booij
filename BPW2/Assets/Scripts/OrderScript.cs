9af8656ead481d68e362f7dbb4995a08381f607e
Eise <eise.booij@gmail.com>
2022-04-01 19:15:18 +0200

    More finishing touches

--- BPW2/Assets/Scripts/OrderScript.cs
+++ BPW2/Assets/Scripts/OrderScript.cs
@@ -11,6 +11,7 @@ public class OrderScript : MonoBehaviour
     public int SecondaryIngredientMaxStrenght;
     public int Character; //5 is for doesnt matter
     public TextMeshProUGUI OrderText;
+    public Material mat;
 
     public string order;
     public string success;
@@ -29,36 +30,38 @@ public class OrderScript : MonoBehaviour
         if (accepted == true)
         {
             OrderText.text = success;
+            mat.SetColor("_Color", Color.green);
         }
         else
         {
             OrderText.text = fail;
+            mat.SetColor("_Color", Color.red);
         }
         
     }
 
     public void CheckOrder(int first, int firstvalue, int second, int secondvalue, int character)
     {
-        if (MainIngredient != 5)
-        {
+      //  if (MainIngredient != 5)
+       // {
             if (MainIngredient != first || firstvalue > MainIngredientMaxStrenght)
             {
                 accepted = false;
             }
-        }
-        if (SecondaryIngredient != 5)
-        {
-            if (SecondaryIngredient != second || secondvalue > SecondaryIngredientMaxStrenght)
-            {
-                accepted = false;
-            }
-        }
-        if (Character != 5)
-        {
+       // }
+       // if (SecondaryIngredient != 5)
+       // {
+       //     if (SecondaryIngredient != second || secondvalue > SecondaryIngredientMaxStrenght)
+       //     {
+       //         accepted = false;
+       //     }
+       // }
+        //if (Character != 5)
+       // {
             if (Character != character)
             {
                 accepted = false;
             }
-        }
+       // }
     }
 }
